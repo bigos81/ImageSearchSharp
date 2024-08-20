5d4229356c5f19de5927c7785c703ff195d26cf1
bigos81 <michal.rosikiewicz@gmail.com>
2024-08-20 10:39:08 +0200

    fixed imports and namespaces

--- ImageSearchHelpers.cs
+++ ImageSearchHelpers.cs
@@ -1,15 +1,18 @@
 ﻿using Point = OpenCvSharp.Point;
 
-internal static class ImageSearchHelpers
+namespace ImageSearchSharp
 {
-
-    public static bool ContainsCloseEnoughPoint(List<Point> list, Point point, int xDistance, int yDistance)
+    internal static class ImageSearchHelpers
     {
-        foreach (Point cPoint in list)
-        {
-            if (Math.Abs(cPoint.X - point.X) < xDistance && Math.Abs(cPoint.Y - point.Y) < yDistance) { return true; }
-        }
 
-        return false;
+        public static bool ContainsCloseEnoughPoint(List<Point> list, Point point, int xDistance, int yDistance)
+        {
+            foreach (Point cPoint in list)
+            {
+                if (Math.Abs(cPoint.X - point.X) < xDistance && Math.Abs(cPoint.Y - point.Y) < yDistance) { return true; }
+            }
+
+            return false;
+        }
     }
 }
